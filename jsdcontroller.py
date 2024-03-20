a3813f574f31a099c0928f840580181ca35d4a62
rtomek <47761173+rtomek@users.noreply.github.com>
2024-03-20 14:11:29 -0500

    Refactoring to prep for new UI elements

--- jsdcontroller.py
+++ jsdcontroller.py
@@ -11,44 +11,44 @@ from scipy.spatial import distance
 
 
 class JSDController(QObject):
-    plotdatachanged = Signal()
+    categoryplotdatachanged = Signal()
     def __init__(self, jsd_view, jsd_model):
         super().__init__()
         self.jsd_view = jsd_view
         self.jsd_model = jsd_model
 
         self.fileChanged(None, newcategoryindex=2)
-        for f_c in self.jsd_view.groupbox.file_comboboxes:
+        for f_c in self.jsd_view.dataselectiongroupbox.file_comboboxes:
             f_c.currentIndexChanged.connect(self.fileChanged)
-        self.jsd_view.groupbox.category_combobox.currentIndexChanged.connect(self.categoryChanged)
+        self.jsd_view.dataselectiongroupbox.category_combobox.currentIndexChanged.connect(self.categoryChanged)
 
     def fileChanged(self, _, newcategoryindex = None):
-        categoryindex = self.jsd_view.groupbox.category_combobox.currentIndex()
+        categoryindex = self.jsd_view.dataselectiongroupbox.category_combobox.currentIndex()
         if newcategoryindex is not None:
             categoryindex = newcategoryindex
 
-        cbox0 = self.jsd_view.groupbox.file_comboboxes[0]
+        cbox0 = self.jsd_view.dataselectiongroupbox.file_comboboxes[0]
         categorylist = list(self.jsd_model.raw_data[cbox0.currentData()].sheets.keys())
 
-        for cbox2 in self.jsd_view.groupbox.file_comboboxes[1:]:
+        for cbox2 in self.jsd_view.dataselectiongroupbox.file_comboboxes[1:]:
             categorylist2 = self.jsd_model.raw_data[cbox2.currentData()].sheets.keys()
             categorylist = [value for value in categorylist if value in categorylist2]
 
-        self.jsd_view.groupbox.category_combobox.blockSignals(True)
-        self.jsd_view.groupbox.category_combobox.clear()
-        self.jsd_view.groupbox.category_combobox.addItems(categorylist)
-        self.jsd_view.groupbox.category_combobox.setCurrentIndex(categoryindex)
-        self.jsd_view.groupbox.category_combobox.blockSignals(False)
+        self.jsd_view.dataselectiongroupbox.category_combobox.blockSignals(True)
+        self.jsd_view.dataselectiongroupbox.category_combobox.clear()
+        self.jsd_view.dataselectiongroupbox.category_combobox.addItems(categorylist)
+        self.jsd_view.dataselectiongroupbox.category_combobox.setCurrentIndex(categoryindex)
+        self.jsd_view.dataselectiongroupbox.category_combobox.blockSignals(False)
 
         self.categoryChanged()
 
     def categoryChanged(self):
-        cat = self.jsd_view.groupbox.category_combobox.currentText()
-        # cbox0 = self.jsd_view.groupbox.file_comboboxes[0]
+        cat = self.jsd_view.dataselectiongroupbox.category_combobox.currentText()
+        # cbox0 = self.jsd_view.dataselectiongroupbox.file_comboboxes[0]
 
         # cols = self.jsd_model.raw_data[cbox0.currentData()].sheets[cat].columns.values()
         # We don't really care about the columns themselves, we just need to update the jsd plot
-        num_files = len(self.jsd_view.groupbox.file_comboboxes)
+        num_files = len(self.jsd_view.dataselectiongroupbox.file_comboboxes)
         self.jsd_model.input_data.clear()
         self.jsd_model.row_count = int(num_files * (num_files - 1) / 2)
         #self.jsd_model.input_data = [[0 for i in range(2)] for j in range(self.jsd_model.col_count)]
@@ -56,7 +56,7 @@ class JSDController(QObject):
         # data_vec = [0] * int(num_files * (num_files + 1) / 2)
         # cur_col = 0
         for i in range(0, num_files):
-            cbox1 = self.jsd_view.groupbox.file_comboboxes[i]
+            cbox1 = self.jsd_view.dataselectiongroupbox.file_comboboxes[i]
             df1 = self.jsd_model.raw_data[cbox1.currentData()].sheets[cat].df
             cols_to_use = self.jsd_model.raw_data[cbox1.currentData()].sheets[cat].data_columns
 
@@ -68,7 +68,7 @@ class JSDController(QObject):
                 cols_to_use.append('Not reported')
 
             for j in range(i+1, num_files):
-                cbox2 = self.jsd_view.groupbox.file_comboboxes[j]
+                cbox2 = self.jsd_view.dataselectiongroupbox.file_comboboxes[j]
                 df2 = self.jsd_model.raw_data[cbox2.currentData()].sheets[cat].df
                 # cols_to_use = df1.columns.intersection(df2.columns.values())
                 # cols_to_use = cols_to_use[1:] # First column is date
@@ -99,7 +99,7 @@ class JSDController(QObject):
                 self.jsd_model.row_count = len(date_list) # This shouldn't be necessary as column_count isn't used
 
             # cur_col += 2
-        self.plotdatachanged.emit()
+        self.categoryplotdatachanged.emit()
         self.jsd_model.layoutChanged.emit()
 
 
--- jsdview.py
+++ jsdview.py
@@ -13,100 +13,110 @@ class JsdWindow(QMainWindow):
         super().__init__()
 
         # Set up graphical layout
-        self.groupbox = JsdGroupBox()
+        self.dataselectiongroupbox = JsdDataSelectionGroupBox()
 
-        self.table_hbox = QHBoxLayout()
         self.table_view = QTableView()
-        self.table_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.table_view.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        self.chart = QChart()
-        self.chart.setAnimationOptions(QChart.AllAnimations)
-        self.chart_view = QChartView(self.chart)
-        self.chart_view.setRenderHint(QPainter.Antialiasing)
-        self.chart_view.setMinimumSize(800, 600)
+        self.jsd_timeline_chart = QChart()
+        self.jsd_timeline_chart.setAnimationOptions(QChart.AllAnimations)
+        self.jsd_timeline_chart_view = QChartView(self.jsd_timeline_chart)
+        self.jsd_timeline_chart_view.setRenderHint(QPainter.Antialiasing)
+        self.jsd_timeline_chart_view.setMinimumSize(800, 600)
 
         self.setCentralWidget(QWidget())
-        self.main_layout = QVBoxLayout()
-        self.main_layout.addWidget(self.groupbox)
-        #self.main_layout.addWidget(self.chart_view)
-        self.table_hbox.addWidget(self.table_view)
-        self.table_hbox.addWidget(self.chart_view, stretch=1)
-        self.main_layout.addLayout(self.table_hbox)
-        self.centralWidget().setLayout(self.main_layout)
+        self.centralWidget().setLayout(self.createMainLayout())
 
-        self.menu_bar = QMenuBar()
-        fileMenu = self.menu_bar.addMenu("File")
-        settingsMenu = self.menu_bar.addMenu("Settings")
-        self.chartanimationsetting = QAction("Chart Animations", self)
-        self.chartanimationsetting.setCheckable(True)
-        self.chartanimationsetting.setChecked(True)
-        self.chartanimationsetting.toggled.connect(self.setAnimationOptions)
-        settingsMenu.addAction(self.chartanimationsetting)
-        self.setMenuBar(self.menu_bar)
+        self.setMenuBar(self.createMenuBar())
 
         # Connect model and controller to this view
         self.jsd_model = jsdmodel.JSDTableModel()
         self.jsd_controller = jsdcontroller.JSDController(self, self.jsd_model)
-        self.jsd_controller.plotdatachanged.connect(self.updateplot)
+        self.jsd_controller.categoryplotdatachanged.connect(self.updateCategoryPlots)
         # The plotdatachanged signal emits before we connect it, so call updateplot() once here
-        self.updateplot()
+        self.updateCategoryPlots()
 
 
         self.setWindowTitle('MIDRC JSD Tool')
 
-    def updateplot(self):
+    def createMainLayout(self):
+        table_hbox = QHBoxLayout()
+        self.table_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table_view.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.dataselectiongroupbox)
+        # main_layout.addWidget(self.chart_view)
+        table_hbox.addWidget(self.table_view)
+        table_hbox.addWidget(self.jsd_timeline_chart_view, stretch=1)
+        main_layout.addLayout(table_hbox)
+        return main_layout
+
+    def createMenuBar(self):
+        menu_bar = QMenuBar()
+        fileMenu = menu_bar.addMenu("File")
+        settingsMenu = menu_bar.addMenu("Settings")
+        chartanimationsetting = QAction("Chart Animations", self)
+        chartanimationsetting.setCheckable(True)
+        chartanimationsetting.setChecked(True)
+        chartanimationsetting.toggled.connect(self.setAnimationOptions)
+        settingsMenu.addAction(chartanimationsetting)
+        return menu_bar
+
+    def updateCategoryPlots(self):
+        self.updatejsdtimelineplot()
+
+    def updatejsdtimelineplot(self):
         # print("Updating table and plot")
         self.table_view.setModel(self.jsd_model)
 
-        self.chart.removeAllSeries()
-        self.series = QLineSeries()
-        self.series.setName('{} vs {} {} JSD'.format(self.groupbox.file_comboboxes[0].currentData(),
-                                                     self.groupbox.file_comboboxes[1].currentData(),
-                                                     self.groupbox.category_combobox.currentText()))
+        self.jsd_timeline_chart.removeAllSeries()
+        series = QLineSeries()
+        series.setName('{} vs {} {} JSD'.format(self.dataselectiongroupbox.file_comboboxes[0].currentData(),
+                                                     self.dataselectiongroupbox.file_comboboxes[1].currentData(),
+                                                     self.dataselectiongroupbox.category_combobox.currentText()))
         col = 0
         for i in range(self.jsd_model.rowCount()):
             if self.jsd_model.input_data[i][col] is not None:
-                self.series.append(QDateTime(self.jsd_model.input_data[i][col], QTime()).toMSecsSinceEpoch(),
-                                   self.jsd_model.input_data[i][col + 1])
+                series.append(QDateTime(self.jsd_model.input_data[i][col], QTime()).toMSecsSinceEpoch(),
+                              self.jsd_model.input_data[i][col + 1])
         # self.mapper = QVXYModelMapper(self)
         # self.mapper.setXColumn(0)
         # self.mapper.setYColumn(1)
-        # self.mapper.setSeries(self.series)
+        # self.mapper.setSeries(series)
         # self.mapper.setModel(self.jsd_model)
-        self.chart.addSeries(self.series)
+        self.jsd_timeline_chart.addSeries(series)
 
 
-        self.jsd_model.add_mapping(self.series.pen().color().name(),
-                               QRect(0, 0, 2, self.jsd_model.rowCount()))
+        self.jsd_model.add_mapping(series.pen().color().name(),
+                                   QRect(0, 0, 2, self.jsd_model.rowCount()))
         # self.chart.createDefaultAxes()
 
-        for axis in self.chart.axes():
-            # self.series.detachAxis(axis)
-            self.chart.removeAxis(axis)
+        for axis in self.jsd_timeline_chart.axes():
+            # series.detachAxis(axis)
+            self.jsd_timeline_chart.removeAxis(axis)
 
         axisX = QDateTimeAxis()
         axisX.setTickCount(10)
         axisX.setFormat("MMM yyyy")
         axisX.setTitleText("Date")
-        self.chart.addAxis(axisX, Qt.AlignBottom)
-        self.series.attachAxis(axisX)
+        self.jsd_timeline_chart.addAxis(axisX, Qt.AlignBottom)
+        series.attachAxis(axisX)
 
         axisY = QValueAxis()
         axisY.setTitleText("JSD value")
         axisY.setRange(0,1)
         axisY.setTickCount(11)
-        self.chart.addAxis(axisY, Qt.AlignLeft)
-        self.series.attachAxis(axisY)
+        self.jsd_timeline_chart.addAxis(axisY, Qt.AlignLeft)
+        series.attachAxis(axisY)
 
     def setAnimationOptions(self, toggle):
         # print(f"Set Chart Animations To {toggle}")
         if toggle:
-            self.chart.setAnimationOptions(QChart.AllAnimations)
+            self.jsd_timeline_chart.setAnimationOptions(QChart.AllAnimations)
         else:
-            self.chart.setAnimationOptions(QChart.NoAnimation)
+            self.jsd_timeline_chart.setAnimationOptions(QChart.NoAnimation)
 
-class JsdGroupBox(QGroupBox):
+class JsdDataSelectionGroupBox(QGroupBox):
     def __init__(self):
         super().__init__()
 
