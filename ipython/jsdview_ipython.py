3f5d496c80cb17589d7aa5a36310e001abaf3b9d
rtomek <rtomek@uchicago.edu>
2024-11-26 19:39:29 -0600

    Refactoring and layout tweaks

--- ipython/jsdview_ipython.py
+++ ipython/jsdview_ipython.py
@@ -1,135 +1,14 @@
-from PySide6.QtCore import Signal, QObject, QDate
+from PySide6.QtCore import Signal, QDate
 import matplotlib.pyplot as plt
 import seaborn as sns
 import ipywidgets as widgets
-from IPython.display import display
-from ipywidgets import VBox, HBox, Label
 import numpy as np
 import pandas as pd
 
-from jsdview_base import JsdViewBase, GroupBoxData
+from jsdview_base import JsdViewBase
 from jsdmodel import JSDTableModel
+from ipython.dataselectiongroupbox import DataSelectionGroupBox
 
-
-
-# Define the data selection group box
-class DataSelectionGroupBox(QObject, GroupBoxData):
-    category_changed = Signal()
-    file_selection_changed = Signal(str)
-
-    def __init__(self, jsd_model):
-        super().__init__()
-        self.jsd_model = jsd_model
-        self._file_comboboxes = []
-        self._category_combobox = widgets.Dropdown(
-            options=[],
-            value=None,
-            description='Attribute:',
-            disabled=True,
-        )
-        self.layout = VBox()
-        self._setup_layout()
-
-        self.num_fileboxes = 2
-        self._initialize_data_sources()
-
-        # Connect signals
-        self._category_combobox.observe(self.on_category_changed, names='value')
-        for combobox in self.file_comboboxes:
-            combobox.observe(self.on_file_selection_changed, names='value')
-
-    def _setup_layout(self):
-        self.layout.children = [self._category_combobox]
-
-    def _initialize_data_sources(self):
-        # Initialize file comboboxes based on data sources from JSDController
-        data_sources = self.jsd_model.data_sources
-        for index, data_source_key in enumerate(list(data_sources.keys())[len(self.file_comboboxes):self.num_fileboxes]):
-            combobox = widgets.Dropdown(
-                options=list([ds_key for ds_key in data_sources.keys()]),
-                value=data_source_key,
-                description=f'Data Source {index + 1}:',
-                disabled=False
-            )
-            self.file_comboboxes.append(combobox)
-            self.layout.children += (combobox,)
-            combobox.observe(self.on_file_selection_changed, names='value')
-
-        # Initialize category combobox based on the current data selection
-        if self.file_comboboxes:
-            self.on_file_selection_changed()
-
-    def update_file_comboboxes(self):
-        # Update file comboboxes based on data sources
-        data_sources = self.jsd_model.data_sources
-        for combobox in self._file_comboboxes:
-            combobox.options = list(data_sources.keys())
-        if self.num_fileboxes > len(self._file_comboboxes):
-            self._initialize_data_sources()
-
-    @property
-    def file_comboboxes(self):
-        return self._file_comboboxes
-
-    def change_number_of_fileboxes(self, num_fileboxes):
-        self.num_fileboxes = num_fileboxes
-
-        # Add file comboboxes if num_fileboxes is greater than the current number
-        if self.num_fileboxes > len(self.file_comboboxes):
-            self.update_file_comboboxes()
-
-        # Remove file comboboxes if num_fileboxes is less than the current number
-        elif self.num_fileboxes < len(self.file_comboboxes):
-            # Remove excess comboboxes from the layout and list
-            excess_comboboxes = len(self.file_comboboxes) - self.num_fileboxes
-            for _ in range(excess_comboboxes):
-                combobox_to_remove = self._file_comboboxes.pop()
-                self.layout.children = tuple(child for child in self.layout.children if child != combobox_to_remove)
-            self.on_file_selection_changed()
-
-    def update_category_combobox(self):
-        # Update category combobox based on selected data sources
-        previous_value = self.get_category_info()['current_text']
-
-        file_infos = self.get_file_infos()
-        cbox0 = file_infos[0]
-        common_categories = self.jsd_model.data_sources[cbox0['source_id']].sheets.keys()
-
-        for cbox2 in file_infos[1:]:
-            categorylist2 = self.jsd_model.data_sources[cbox2['source_id']].sheets.keys()
-            common_categories = [value for value in common_categories if value in categorylist2]
-        self._category_combobox.options = common_categories
-        if previous_value not in common_categories:
-            self.update_category_list(common_categories, 0)
-            self._category_combobox.index = 0
-        else:
-            self.update_category_list(common_categories, common_categories.index(previous_value))
-            self._category_combobox.value = previous_value
-        self._category_combobox.disabled = False
-
-    def on_category_changed(self, change):
-        self.update_category_text(change['new'])
-        self.category_changed.emit()
-
-    def on_file_selection_changed(self, change=None):
-        data_sources = self.jsd_model.data_sources
-        file_infos = []
-        for index, file_combobox in enumerate(self.file_comboboxes):
-            data_source_dict = data_sources[file_combobox.value].data_source
-            file_infos.append({
-                'description': data_source_dict['description'],
-                'source_id': data_source_dict['name'],
-                'index': index,
-                'checked': True,
-            })
-        self._file_infos = file_infos
-        self.update_category_combobox()
-        self.file_selection_changed.emit(change)
-
-    def display(self):
-        return self.layout
-
-# Create instance of DataSelectionGroupBox
 
 class JsdViewIPython(JsdViewBase):
     jsd_timeline_plot_update = Signal(JSDTableModel)
@@ -148,8 +27,6 @@ class JsdViewIPython(JsdViewBase):
         self.jsd_model.add_data_source(data_source_dict)
 
     def update_jsd_timeline_plot(self, jsd_model):
-        print("Plotting timeline chart using Seaborn...")
-
         # Initialize plot_data to None for proper handling of the first iteration
         plot_data = None
 
@@ -208,11 +85,7 @@ class JsdViewIPython(JsdViewBase):
             plt.show()
             plt.close()
 
-        print("Plotting complete.")
-
     def update_area_chart(self, sheet_dict):
-        print("Plotting area charts...")
-
         category = self.dataselectiongroupbox.get_category_info()['current_text']
 
         # Set up the figure with multiple subplots
@@ -267,5 +140,3 @@ class JsdViewIPython(JsdViewBase):
             self.output_area_chart.clear_output(wait=True)
             plt.show()
             plt.close()
-
-        print('done with this step')
